7c27a337b61b03f531877dd57c7fe0e0f30dfb7f
Delesoo <strongtoka@gmail.com>
2023-11-08 20:33:06 +0400

    connection mongo

--- api/index.js
+++ api/index.js
@@ -1,15 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/User');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+   .then(()=>{
+    console.log("connection successful !")
+   }).catch((err)=>{
+    console.log("unsuccessful connection")
+   });
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req,res) => {
-
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    try {
+        const createdUser = await User.create({username,password});
+        jwt.sign({userId:createdUser._id}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json('ok');
+        });
+    } catch(err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
 });
 
 app.listen(4040);
-
-//mZvRGm0tJDnHlqP9
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import Register from "./Register"
+import axios from 'axios';
 
 function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
+  
   return (
     <Register />
   )
