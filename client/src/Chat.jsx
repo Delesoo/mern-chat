5e172892265dee5db635de01c5103765b89dc115
Delesoo <strongtoka@gmail.com>
2023-11-15 20:54:15 +0400

    selecting conversation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import {UserContext} from './UserContext.jsx'
 
 export default function Chat() {
     const [ws,setWs] = useState(null);
     const [onlinePeople,setOnlinePeople] = useState({});
+    const [selectedUserId,setSelectedUserId] = useState(null);
+    const {username,id} = useContext(UserContext);
     useEffect(() => {
        const ws = new WebSocket('ws://localhost:4040');
        setWs(ws);
@@ -22,24 +26,35 @@ export default function Chat() {
             showOnlinePeople(messageData.online);
         }
     }
+
+    const onlinePeopleExclOurUser = {...onlinePeople};
+    delete onlinePeopleExclOurUser[id]
+
     return (
         <div className="flex h-screen">
-            <div className="bg-white-100 w-1/3 pl-4 pt-4 mb-4">
-                <div className="text-cyan-500 font-bold flex gap-2 mb-4"> 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-</svg>
-                 NoiceChat
-                </div>
-                {Object.keys(onlinePeople).map(userId => (
-                <div className="border-b border-gray-100 py-2 flex items-center gap-2 cursor-pointer">
-                    <Avatar username={onlinePeople[userId]} userId={userId} />
-                    <span className="text-gray-800">{onlinePeople[userId]}</span>              
+            <div className="bg-white-100 w-1/3">
+                <Logo />
+                {Object.keys(onlinePeopleExclOurUser).map(userId => (
+                <div key={userId} onClick={() => setSelectedUserId(userId)} 
+                    className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-100' : '')}>
+                        {userId === selectedUserId && (
+                            <div className="w-1 bg-cyan-500 h-12 rounded-r-md"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-4 items-center">
+                        <Avatar username={onlinePeople[userId]} userId={userId} />
+                    <span className="text-gray-800">{onlinePeople[userId]}</span> 
+                        </div>           
                 </div>
             ))}
             </div>
             <div className="flex flex-col bg-cyan-50 w-2/3 p-2">
-                <div className="flex-grow">messages with selected person</div>
+                <div className="flex-grow">
+                    {!selectedUserId && (
+                    <div className="flex h-full flex-grow items-center justify-center">
+                        <div className="text-gray-400">&larr; Select a person</div>
+                    </div>
+                )}
+                </div>
                 <div className="flex gap-2">
                     <input type="text" placeholder="Type your message here" className="bg-white flex-grow border rounded-md p-2" />
                     <button className="bg-cyan-500 rounded-md p-2 text-white">
