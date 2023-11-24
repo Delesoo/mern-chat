6426633e8e2410314f2751ace63571c50ca56cdb
Delesoo <strongtoka@gmail.com>
2023-11-24 22:55:25 +0400

    logout button & displaying usernames

--- api/index.js
+++ api/index.js
@@ -88,6 +88,10 @@ app.post('/login', async (req,res) => {
     }
 });
 
+app.post('/logout', (req,res) => {
+    res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
+})
+
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     try {
@@ -124,6 +128,7 @@ wss.on('connection', (connection, req) => {
         connection.ping();
         connection.deathTimer = setTimeout(() => {
             connection.isAlive = false;
+            clearInterval(connection.timer);
             connection.terminate();
             notifyAboutOnlinePeople();
             console.log('dead');
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -12,7 +12,7 @@ export default function Chat() {
     const [offlinePeople,setOfflinePeople] = useState({});
     const [selectedUserId,setSelectedUserId] = useState(null);
     const [newMessageText, setNewMessageText] = useState('');
-    const {username,id} = useContext(UserContext);
+    const {username,id,setId,setUsername} = useContext(UserContext);
     const [messages,setMesssages] = useState([]);
     const divUnderMessages = useRef();
     useEffect(() => {
@@ -44,6 +44,13 @@ export default function Chat() {
         } else if('text' in messageData) {
             setMesssages(prev => ([...prev, {...messageData}]));
         }
+    }
+    function logout() {
+        axios.post('/logout').then(() => {
+            setWs(null);
+            setId(null);
+            setUsername(null);
+        });
     }
     function sendMessage(ev) {
         ev.preventDefault();
@@ -96,7 +103,8 @@ export default function Chat() {
 
     return (
         <div className="flex h-screen">
-            <div className="bg-white-100 w-1/3">
+            <div className="bg-white-100 w-1/3 flex flex-col">
+                <div className="flex-grow">
                 <Logo />
                 {Object.keys(onlinePeopleExclOurUser).map(userId => (
                 <Contact 
@@ -118,6 +126,19 @@ export default function Chat() {
                          selected={userId === selectedUserId}
                 />
             ))}
+                </div>
+                <div className="p-2 text-center flex items-center justify-center">
+                    <span className="mr-2 text-sm text-gray-600 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+                        {username}</span>
+                    <button 
+                        onClick={logout}
+                        className="text-sm bg-cyan-100 py-1 px-2 text-gray-500 border rounded-md">logout</button>
+
+                </div>
             </div>
             <div className="flex flex-col bg-cyan-50 w-2/3 p-2">
                 <div className="flex-grow">
